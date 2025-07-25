1fbc62dcf63486158275c083957c0b2e2140fe8a
Petr Fifka <petr.fifka@tredgate.cz>
2025-07-25 11:01:15 +0200

    typescript introduction

---
+++ src/learning/type_annotions.ts
@@ -0,0 +1,11 @@
+// type_annotions.ts
+// src/learning
+let firstName: string = "Petr";
+console.log(firstName);
+
+firstName = "Pavel";
+console.log(firstName);
+
+// ? Nelze přiřadit number do string proměnné
+// firstName = 5;
+// console.log(firstName);
